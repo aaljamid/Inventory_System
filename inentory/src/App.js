03c6175a17b5b13426c9e00c8576da1ce1db84f8
aLi <aaljamid@emich.edu>
2020-02-23 19:37:39 +0300

    adding the all the pages and putting the structure + adding bootstrab, axios, and time API

---
+++ inentory/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import Inventory from "./Components/Inventory";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <footer>
+        <h1>Footer</h1>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ inentory/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      //  main div for the image, it gets the information
+      //  from the parent props
+      <div>
+        <h1>Home</h1>
+      </div>
+    );
+  }
+}
