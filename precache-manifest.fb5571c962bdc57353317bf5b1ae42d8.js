36d55a79d4324f2bc7f14a5afea7fe2bd3cdc0c1
aLi <aaljamid@emich.edu>
2020-02-26 16:05:54 +0300

    Updates

---
+++ precache-manifest.fb5571c962bdc57353317bf5b1ae42d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208cdc50119187f7d0e9630397a18a7f",
+    "url": "/Inventory_System/index.html"
+  },
+  {
+    "revision": "914d4d787c7e4ea0caef",
+    "url": "/Inventory_System/static/css/2.3db4eb7d.chunk.css"
+  },
+  {
+    "revision": "cf3339d500898c41408c",
+    "url": "/Inventory_System/static/css/main.fa09abfb.chunk.css"
+  },
+  {
+    "revision": "914d4d787c7e4ea0caef",
+    "url": "/Inventory_System/static/js/2.9e829f27.chunk.js"
+  },
+  {
+    "revision": "6ed693e63a6f71290571a68253c0a1b1",
+    "url": "/Inventory_System/static/js/2.9e829f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf3339d500898c41408c",
+    "url": "/Inventory_System/static/js/main.3245c219.chunk.js"
+  },
+  {
+    "revision": "5d42efa64fe3777bd550",
+    "url": "/Inventory_System/static/js/runtime-main.14dbee51.js"
+  },
+  {
+    "revision": "47bb19417193abdc1242e1e1f30d3b9d",
+    "url": "/Inventory_System/static/media/stock.47bb1941.png"
+  }
+]);
