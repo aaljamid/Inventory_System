f3e00f3b847da3351d2f329ff0b46f61cb480eea
aLi <aaljamid@emich.edu>
2020-02-26 10:38:01 +0300

    fixing the issue of the bootstrap

--- inventory/src/Components/NavBar.js
+++ inventory/src/Components/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
+import bootstrap from "bootstrap";
 // import the inventory and gome classess
 import Inventory from "./Inventory";
 import Home from "./Home";
